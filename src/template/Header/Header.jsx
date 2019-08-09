29ec9a7ce1cb345023dd013d8df4613b3be33c2e
jhserodio <jhserodio@gmail.com>
2019-08-08 22:12:08 -0300

    feat (template/Snackbar): adjust snackbar

--- src/template/Header/Header.jsx
+++ src/template/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import style from './Header.module.css';
+import { Logo } from '../../components/Logo/Logo';
 
-const Header = () => <header className={style.Header}>header</header>;
+const Header = () => (
+  <header className={style.Header}>
+    <Logo />
+  </header>
+);
 
 export { Header };
