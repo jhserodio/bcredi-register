3149b03c934153c28396a0fcc5b7c72fae37b4cc
jhserodio <jhserodio@gmail.com>
2019-08-08 22:37:43 -0300

    feat (pages/Home): add messages

--- src/pages/Home/intl_en_US.js
+++ src/pages/Home/intl_en_US.js
@@ -1,5 +1,38 @@
 const home = {
-  title: 'Pagina inicial',
+  title: 'Create my registration',
+  message:
+    'To track your credit hiring you will use your email and social security number.',
+  form: {
+    name: {
+      label: 'Name',
+      placeholder: 'Write your full name',
+    },
+    cpf: {
+      label: 'CPF',
+    },
+    birthDay: {
+      label: 'Birth day',
+    },
+    pass: {
+      name: 'Password',
+      placeholder: 'create your password',
+    },
+    visibility: {
+      view: 'view password',
+      hide: 'hide password',
+    },
+    policy: {
+      phrase: 'I have read and agree to the',
+      privacy: 'Privacy Policy',
+      and: 'and',
+      use: 'Information Use Policy',
+    },
+    submit: 'Registry',
+  },
+  footer: {
+    message: 'I already made my registration',
+    link: 'Sign in now',
+  },
 };
 
 export { home };
--- src/pages/Home/intl_pt_BR.js
+++ src/pages/Home/intl_pt_BR.js
@@ -1,5 +1,38 @@
 const home = {
-  title: 'Home page',
+  title: 'Criar meu cadastro',
+  message:
+    'Para acompanhar sua contratação de crédito você utilizará seu e-mail e CPF.',
+  form: {
+    name: {
+      label: 'Nome',
+      placeholder: 'Escreva seu nome completo',
+    },
+    cpf: {
+      label: 'CPF',
+    },
+    birthDay: {
+      label: 'Data de nascimento',
+    },
+    pass: {
+      name: 'Senha',
+      placeholder: 'Cadastre uma senha',
+    },
+    visibility: {
+      view: 'ver senha',
+      hide: 'esconder senha',
+    },
+    policy: {
+      phrase: 'Li e estou de acordo com a',
+      privacy: 'Política de Privacidade',
+      and: 'e a',
+      use: 'Política de Uso de Informações',
+    },
+    submit: 'Cadastrar',
+  },
+  footer: {
+    message: 'Já fiz meu cadastro',
+    link: 'Entrar agora',
+  },
 };
 
 export { home };
